d5ce84cc5c446b17d0cbe11d93c97b13c8833456
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2016-07-22 14:22:09 -0700

    Install apps and set up urls

--- fe_now/urls.py
+++ fe_now/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^prices/', include('prices.urls')),
+    url(r'^recipes/', include('recipes.urls')),
+    url(r'^schedule/', include('schedule.urls')),
     url(r'^admin/', admin.site.urls),
 ]
