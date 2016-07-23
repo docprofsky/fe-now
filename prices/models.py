1709cba5da94b7afb56d682c39dd0956784caa14
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2016-07-23 11:42:29 -0700

    Reorder

--- prices/models.py
+++ prices/models.py
@@ -4,10 +4,11 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 class Item(models.Model):
-    price = models.DecimalField(decimal_places=2, max_digits=5, validators=[MinValueValidator(0)])
-    quantity = models.IntegerField(validators=[MinValueValidator(0)])
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
+    price = models.DecimalField(decimal_places=2, max_digits=5, validators=[MinValueValidator(0)])
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
+
 
     def __str__(self):
         return self.name
